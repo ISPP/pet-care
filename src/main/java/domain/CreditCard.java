e496cbeb2a7661b28de2c9bbb2939715d9998f83
MoisesLlamasGarcía <moisesllamasgarcia@gmailcom>
2016-03-29 19:11:19 +0200

    ADD: administrator and customer

---
+++ src/main/java/domain/CreditCard.java
@@ -0,0 +1,92 @@
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
+
+
+@Access(AccessType.PROPERTY)
+@Embeddable
+public class CreditCard {
+	//Constructor-------------------------------------------------
+			public CreditCard(){
+				super();
+			}
+			
+			//Attributes---------------------------------------------
+			private String holderName, brandName, number;
+			private int expirationMonth, expirationYear, CVV;
+
+			@NotBlank
+			@SafeHtml(whitelistType = WhiteListType.NONE)
+			public String getHolderName() {
+				return holderName;
+			}
+
+			public void setHolderName(String holderName) {
+				this.holderName = holderName;
+			}
+
+			@NotBlank
+			@SafeHtml(whitelistType = WhiteListType.NONE)
+			public String getBrandName() {
+				return brandName;
+			}
+
+			public void setBrandName(String brandName) {
+				this.brandName = brandName;
+			}
+			@NotBlank
+			@CreditCardNumber
+			@SafeHtml(whitelistType = WhiteListType.NONE)
+			public String getNumber() {
+				return number;
+			}
+
+			public void setNumber(String number) {
+				this.number = number;
+			}
+
+			@Range(min = 1, max = 12)
+			public int getExpirationMonth() {
+				return expirationMonth;
+			}
+
+			public void setExpirationMonth(int expirationMonth) {
+				this.expirationMonth = expirationMonth;
+			}
+
+			@Range(min = 2015)
+			public int getExpirationYear() {
+				return expirationYear;
+			}
+
+			public void setExpirationYear(int expirationYear) {
+				this.expirationYear = expirationYear;
+			}
+
+			@Range(min = 100, max = 999)
+			public int getCVV() {
+				return CVV;
+			}
+
+			public void setCVV(int cVV) {
+				CVV = cVV;
+			}
+			
+			
+			//toString---------------------------------------
+			@Override
+			public String toString() {
+				return "CreditCard [holderName=" + holderName + ", brandName="
+						+ brandName + ", number=" + number + ", expirationMonth="
+						+ expirationMonth + ", expirationYear=" + expirationYear
+						+ ", CVV=" + CVV + "]";
+			}
+}
---
+++ src/main/java/domain/Customer.java
@@ -0,0 +1,74 @@
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
+import org.hibernate.validator.constraints.URL;
+
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Customer extends Actor{
+	public Customer(){
+		super();
+	}
+	
+	private String address, description, homePage, contactPhone;
+	private CreditCard creditCard;
+	
+	
+	@SafeHtml(whitelistType = WhiteListType.NONE)
+	@NotBlank
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	@SafeHtml(whitelistType = WhiteListType.NONE)
+	@NotBlank
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@URL
+	public String getHomePage() {
+		return homePage;
+	}
+	public void setHomePage(String homePage) {
+		this.homePage = homePage;
+	}
+	
+	@NotBlank
+	@Pattern(regexp="^\\d+$")
+	public String getContactPhone() {
+		return contactPhone;
+	}
+	public void setContactPhone(String contactPhone) {
+		this.contactPhone = contactPhone;
+	}
+	
+	
+	@NotNull
+	@Valid
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+	
+	
+
+}
