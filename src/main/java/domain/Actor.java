e798c23173c25f8cfa509de4e24f60f16d0990b7
valbarsau <valentinbarquero@gmail.com>
2016-03-29 19:08:52 +0200

    ADD Actor added
    Class Actor added

--- src/main/java/domain/Actor.java
+++ src/main/java/domain/Actor.java
@@ -1,7 +1,31 @@
 package domain;
 import javax.persistence.Entity;
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 @Entity
+@Access(AccessType.PROPERTY)
 public class Actor extends DomainEntity{
+	private String name;
+	private String surname;
+	private String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 
 }
